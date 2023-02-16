0e4a2fe98bb4a609f0c798dd6aaf1fcd1d585255
ptpyip <yippaco123123@gmail.com>
2023-02-16 09:56:43 +0800

    Complete Exercise 3

--- src/main/java/COMP3111_Lab1/Library.java
+++ src/main/java/COMP3111_Lab1/Library.java
@@ -6,7 +6,12 @@ package COMP3111_Lab1;
 public class Library {
 	
 	public static void main(String arg[]) {
-		System.out.println("Hello, World!");
+		// System.out.println("Hello, World!");		-> Exercise 1
+		
+		// Exercise 3
+		Library lib = new Library();
+		if (lib.someLibraryMethod())
+			System.out.println("Hello, World!");
 	}
 	
     public boolean someLibraryMethod() {
